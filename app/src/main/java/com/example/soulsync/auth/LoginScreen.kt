bad9edb61ec27c1451588ca75931487de590c4f1
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-12-29 17:47:57 +0300

    Added basic UI for login screen

--- app/src/main/java/com/example/soulsync/auth/LoginScreen.kt
+++ app/src/main/java/com/example/soulsync/auth/LoginScreen.kt
@@ -1,8 +1,121 @@
 package com.example.soulsync.auth
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Password
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.soulsync.R
 
+
+@Preview(showBackground = true, showSystemUi = true, name="Login Screen")
 @Composable
 fun LoginUser(){
-    //TODO: Implement login logic
+    var email = remember { mutableStateOf("") }
+    var password = remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+    val appFont = FontFamily(Font(R.font.emilys_candy, FontWeight.Normal))
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "Login User",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+//                fontFamily = appFont
+            )
+
+            Spacer(modifier = Modifier.padding(15.dp))
+
+            OutlinedTextField(
+                value = email.value,
+                onValueChange = { email.value = it },
+                label = { Text("Email") },
+                placeholder = { Text("Enter your email") },
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email"
+                    )
+                },
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear Email",
+                        modifier = Modifier.clickable { email.value = "" }
+                    )
+                },
+                modifier = Modifier.size(width = 350.dp, height = 70.dp)
+            )
+
+            Spacer(modifier = Modifier.padding(12.dp))
+
+            OutlinedTextField(
+                value = password.value,
+                onValueChange = { password.value = it },
+                label = { Text("Password") },
+                placeholder = { Text("Enter your password") },
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Password,
+                        contentDescription = "Password"
+                    )
+                },
+                trailingIcon = {
+                    if(showPassword){
+                        Icon(
+                            imageVector = Icons.Filled.Visibility,
+                            contentDescription = "Toggle Password Visibility",
+                            modifier = Modifier.clickable { showPassword = !showPassword }
+                        )
+                    }else{
+                        Icon(
+                            imageVector = Icons.Filled.VisibilityOff,
+                            contentDescription = "Toggle Password Visibility",
+                            modifier = Modifier.clickable { showPassword = !showPassword }
+                        )
+                    }
+                },
+                visualTransformation = if(showPassword){
+                    VisualTransformation.None
+                }else{
+                    PasswordVisualTransformation()
+                },
+                modifier = Modifier.size(width = 350.dp, height = 70.dp)
+            )
+        }
+    }
 }
