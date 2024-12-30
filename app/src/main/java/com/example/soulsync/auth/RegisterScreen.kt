cd881de01ed6da023a2e64940d5bc3a21bed8f41
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-12-30 04:44:09 +0300

    Added functionality to register a new user

--- app/src/main/java/com/example/soulsync/auth/RegisterScreen.kt
+++ app/src/main/java/com/example/soulsync/auth/RegisterScreen.kt
@@ -1,6 +1,5 @@
 package com.example.soulsync.auth
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,20 +15,23 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -47,23 +49,27 @@ import com.example.soulsync.R
 @Preview(showBackground = true, showSystemUi = true, name="Register Screen")
 @Composable
 fun RegisterUser(
-    onNavigateToLogin: () -> Unit = {}
+    onNavigateToLogin: () -> Unit = {},
+    authViewModel: AuthViewModel = AuthViewModel()
 ){
 
-    val bg_image = painterResource(id = R.drawable.wallpaper_in_purple_aesthetic)
+    val bgImage = painterResource(id = R.drawable.wallpaper_in_purple_aesthetic)
 
-    var email = remember { mutableStateOf("") }
-    var password = remember { mutableStateOf("") }
-    var confirmPassword = remember { mutableStateOf("") }
+    var email = rememberSaveable { mutableStateOf("") }
+    var password = rememberSaveable { mutableStateOf("") }
+    var confirmPassword = rememberSaveable { mutableStateOf("") }
     var showPassword by remember { mutableStateOf(true) }
     val appFont = FontFamily(Font(R.font.emilys_candy, FontWeight.Normal))
+
+    // Observe the registration state using the authViewModel
+    val registerState by authViewModel.registerState.collectAsState()
 
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
             .paint(
-                painter = bg_image,
+                painter = bgImage,
                 contentScale = ContentScale.FillBounds,
                 alpha = 0.4f
             )
@@ -174,7 +180,18 @@ fun RegisterUser(
             Spacer(modifier = Modifier.padding(20.dp))
 
             ElevatedButton(
-                onClick = { /*TODO*/ },
+                onClick = {
+                    if(email.value.isNotEmpty() && password.value.isNotEmpty() && confirmPassword.value.isNotEmpty()){
+                        if(password.value == confirmPassword.value){
+                            authViewModel.registerUser(email.value, password.value)
+                        }else{
+                            authViewModel.setRegisterState("Passwords do not match")
+                        }
+                    }
+                    else{
+                        authViewModel.setRegisterState("!! Please fill in all fields !!")
+                    }
+                },
                 colors = ButtonColors(
                     containerColor = Color(0xFF9279C4),
                     contentColor = Color.White,
@@ -195,7 +212,44 @@ fun RegisterUser(
                 )
             }
 
+            Spacer(modifier = Modifier.padding(10.dp))
 
+            // Display the registration state using the authViewModel
+            when (registerState){
+                is AuthViewModel.RegisterState.Loading -> {
+                    CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+                }
+                // Runs on Failure to Register
+                is AuthViewModel.RegisterState.Error -> {
+                    val errorMessage = (registerState as AuthViewModel.RegisterState.Error).message
+                    Text(
+                        text = errorMessage,
+                        color = Color.Red,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    LaunchedEffect(Unit) {
+                        kotlinx.coroutines.delay(2000)
+                        authViewModel.setRegisterState(message = "")
+                    }
+                }
+                // Run on Successful Registration
+                is AuthViewModel.RegisterState.Success -> {
+                    Text(
+                        text = "Registration successful! Redirecting...",
+                        color = Color.Green,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    LaunchedEffect(Unit) {
+                        kotlinx.coroutines.delay(2000)
+                        // Navigate to the login screen after successful registration
+                        onNavigateToLogin()
+                    }
+
+                }
+                else -> Unit // Initial State
+            }
         }
     }
 }
