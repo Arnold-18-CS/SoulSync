65078b1a6fcb65c6ba351dfada30ddba05bd046e
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2025-01-06 16:43:11 +0300

    Adding new files for each screen in the main application

---
+++ app/src/main/java/com/example/soulsync/ui/quotes/QuotesScreen.kt
@@ -0,0 +1,36 @@
+package com.example.soulsync.ui.quotes
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.soulsync.auth.AuthViewModel
+import com.example.soulsync.ui.theme.BackgroundImage
+import com.example.soulsync.ui.theme.SSPrimaryButton
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun Quotes(onNavigateToStart: () -> Unit = {}) {
+    val authViewModel: AuthViewModel = hiltViewModel()
+    BackgroundImage.Background {
+        Column {
+            Text(text = "Quotes Screen")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            SSPrimaryButton(
+                text = "Logout",
+                onClick = {
+                    authViewModel.logout()
+                    Log.d("AppHome", "User logged out")
+                    onNavigateToStart()
+                },
+            )
+        }
+    }
+}
