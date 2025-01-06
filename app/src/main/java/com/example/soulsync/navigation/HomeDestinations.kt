8c8b5cc350e845cf7238994820f5386aed7d9f2f
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2025-01-06 16:51:33 +0300

    Changing profile to settings and changing icon datatype to Image vector instead of string

--- app/src/main/java/com/example/soulsync/navigation/HomeDestinations.kt
+++ app/src/main/java/com/example/soulsync/navigation/HomeDestinations.kt
@@ -1,17 +1,25 @@
 package com.example.soulsync.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Outbox
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
 sealed class HomeDestinations(
     val route: String,
-    val title: String,
-    val icon: String,
+    val title: String? = null,
+    val icon: ImageVector? = null,
 ) {
-    object Home : HomeDestinations("home", "Home", "Home")
+    object Home : HomeDestinations("home", "Home", Icons.Filled.Home)
 
-    object Quotes : HomeDestinations("quotes", "Quotes", "Checklist")
+    object Quotes : HomeDestinations("quotes", "Quotes", Icons.Filled.Checklist)
 
-    object Memories : HomeDestinations("memories", "Memories", "Image")
+    object Memories : HomeDestinations("memories", "Memories", Icons.Filled.Image)
 
-    object Outbox : HomeDestinations("outbox", "Outbox", "Outbox")
+    object Outbox : HomeDestinations("outbox", "Outbox", Icons.Filled.Outbox)
 
-    object Profile : HomeDestinations("profile", "Profile", "Person")
+    object Settings : HomeDestinations("settings", "Settings", Icons.Filled.Settings)
 }
