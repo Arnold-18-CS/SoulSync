b36d13a4eec10178313200d6245d44d1b8397d49
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2025-01-06 16:37:26 +0300

    Adding new destinations for the main app for each screen

--- app/src/main/java/com/example/soulsync/navigation/HomeDestinations.kt
+++ app/src/main/java/com/example/soulsync/navigation/HomeDestinations.kt
@@ -2,14 +2,16 @@ package com.example.soulsync.navigation
 
 sealed class HomeDestinations(
     val route: String,
+    val title: String,
+    val icon: String,
 ) {
-    object Home : HomeDestinations("home")
+    object Home : HomeDestinations("home", "Home", "Home")
 
-    object Quotes : HomeDestinations("quotes")
+    object Quotes : HomeDestinations("quotes", "Quotes", "Checklist")
 
-    object Memories : HomeDestinations("memories")
+    object Memories : HomeDestinations("memories", "Memories", "Image")
 
-    object Outbox : HomeDestinations("outbox")
+    object Outbox : HomeDestinations("outbox", "Outbox", "Outbox")
 
-    object Profile : HomeDestinations("profile")
+    object Profile : HomeDestinations("profile", "Profile", "Person")
 }
