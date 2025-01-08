1410946f8cc336cc876079c1a6c1410d04ea815e
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2025-01-08 16:35:54 +0300

    Adding logging to show start of the application

--- app/src/main/java/com/example/soulsync/MainActivity.kt
+++ app/src/main/java/com/example/soulsync/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.soulsync
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -22,6 +23,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate called, App started.")
         enableEdgeToEdge()
 //        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
         setContent {
